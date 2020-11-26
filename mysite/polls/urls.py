2da9181e2621dd43d7c4a0fc1f41e2f1fd1a0fd4
Uzair <uzair.z.shaikh@gmail.com>
2020-11-26 22:10:41 +0000

    Initial commit

---
+++ mysite/polls/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'polls'
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('polls/',views.IndexView.as_view(), name ='index'),
+    path('polls/<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('polls/<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('polls/<int:question_id>/vote/', views.vote, name='vote'),
+]
